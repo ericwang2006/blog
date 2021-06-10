771f0dd4a57020921cabdee3a1c67621417a7ad3
ericwang2006 <erciwang2006@gmail.com>
2021-06-11 00:12:25 +0800

    add title

--- net_utils/port_check/port_check.php
+++ net_utils/port_check/port_check.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+	<title>端口测试</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<!-- <meta name="applicable-device" content="mobile">	 -->
 	<meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
